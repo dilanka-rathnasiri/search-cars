29a81c8214a10eea01a75a99ee98b9a11f263f5d
Dilanka Rathnasiri <rdilanka17@gmail.com>
2024-08-18 22:51:15 +0530

    init data-writer

---
+++ data-writer/src/main/java/com/example/data_writer/DataWriterApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataWriterApplication.class, args);
+	}
+
+}
